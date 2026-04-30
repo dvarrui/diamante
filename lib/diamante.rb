54e8621ff716a1201c53d8f59696c4846c4ea742
David Vargas <dvarrui@protonmail.com>
2026-04-30 10:51:59 +0100

    chore: basic skeleton gem

---
+++ lib/diamante.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "diamante/version"
+
+module Diamante
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/diamante/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Diamante
+  VERSION = "0.1.0"
+end
